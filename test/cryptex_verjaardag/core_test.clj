2317ac6dd20bd7f13191b4c0c6a4ffbe25b175bd
Alexander Freeman <alexander.freeman.nl@gmail.com>
2018-11-21 12:37:47 +0100

    Removed old test folder

--- test/cryptex_verjaardag/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns cryptex-verjaardag.core-test
-  (:require [clojure.test :refer :all]
-            [cryptex-verjaardag.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
